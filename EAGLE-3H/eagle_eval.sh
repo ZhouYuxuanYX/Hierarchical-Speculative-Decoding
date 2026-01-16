d754979d917ccf6cb4bfe024d58bcc9c8b1c55c8
kngslwang <kngslwang@gmail.com>
2026-01-16 16:57:39 -0500

    fix eagle

--- EAGLE-3H/eagle_eval.sh
+++ EAGLE-3H/eagle_eval.sh
@@ -1,21 +1,16 @@
 
 # # To run gsm8k with HSD:
-CUDA_VISIBLE_DEVICES=0 python -m eagle.evaluation.gen_ea_answer_llama3chat \
+CUDA_VISIBLE_DEVICES=1 python -m eagle.evaluation.gen_ea_answer_llama3chat \
   --ea-model-path yuhuili/EAGLE3-LLaMA3.1-Instruct-8B \
   --base-model-path meta-llama/Llama-3.1-8B-Instruct \
   --use_eagle3 \
-  --question-end 10 \
+  --bench-name gsm8k \
   --hsd
 
 # # To run gsm8k without HSD:
-CUDA_VISIBLE_DEVICES=0 python -m eagle.evaluation.gen_ea_answer_llama3chat \
+CUDA_VISIBLE_DEVICES=1 python -m eagle.evaluation.gen_ea_answer_llama3chat \
   --ea-model-path yuhuili/EAGLE3-LLaMA3.1-Instruct-8B \
   --base-model-path meta-llama/Llama-3.1-8B-Instruct \
-  --question-end 10 \
-  --use_eagle3
+  --use_eagle3 \
+  --bench-name gsm8k
 
-# # To run gsm8k in baseline:
-CUDA_VISIBLE_DEVICES=0 python -m eagle.evaluation.baseline \
-  --model-path meta-llama/Llama-3.1-8B-Instruct \
-  --bench-name gsm8k \
-  --question-end 10 
