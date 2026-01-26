225e531cc43a7e6cc76b1aab8a1fd905f7056f34
Yuxuan Zhou <43881388+ZhouYuxuanYX@users.noreply.github.com>
2026-01-26 15:39:06 +0800

    Delete chain-of-thought-hub/gsm8k/eval_speculative_qwen_backward.sh

--- chain-of-thought-hub/gsm8k/eval_speculative_qwen_backward.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-cd chain-of-thought-hub-main/gsm8k
-echo "speculative"
-eval "$(/root/anaconda3/bin/conda shell.bash hook)"
-source ~/.bashrc
-source activate base
-conda activate speculative
-conda info
-python3 eval_speculative_decoding_llm.py --backward --speculative
